9f1377bab87f23caf0a2414d3a04620fbad4c434
Ramiz Kichibekov <ramiz161@icloud.com>
2025-05-17 15:48:07 +0400

    Реализация библиотеки

---
+++ PurchaseKit/PurchaseKit/PurchaseKit.h
@@ -0,0 +1,18 @@
+//
+//  PurchaseKit.h
+//  PurchaseKit
+//
+//  Created by Рамиз Кичибеков on 17.05.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PurchaseKit.
+FOUNDATION_EXPORT double PurchaseKitVersionNumber;
+
+//! Project version string for PurchaseKit.
+FOUNDATION_EXPORT const unsigned char PurchaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurchaseKit/PublicHeader.h>
+
+
